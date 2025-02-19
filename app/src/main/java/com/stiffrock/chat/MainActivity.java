56ca50e5c90ec9f41669bac04f64de0e570b8751
Yago <yaguitor@gmail.com>
2025-02-19 13:05:46 +0100

    AutoLogin Working again

--- app/src/main/java/com/stiffrock/chat/MainActivity.java
+++ app/src/main/java/com/stiffrock/chat/MainActivity.java
@@ -1,18 +1,22 @@
 package com.stiffrock.chat;
 
+import static com.stiffrock.chat.utils.LogTag.TAG;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import com.stiffrock.chat.dto.LoginDTO;
-import com.stiffrock.chat.fragments.main.LogInFragment;
 import com.stiffrock.chat.model.CurrentUser;
 import com.stiffrock.chat.model.User;
-import com.stiffrock.chat.network.ApiService;
 import com.stiffrock.chat.network.RetrofitClient;
 import com.stiffrock.chat.utils.FragmentContainerActivity;
 import com.stiffrock.chat.utils.SecureStorage;
@@ -24,51 +28,58 @@ import retrofit2.Response;
 
 public class MainActivity extends FragmentContainerActivity {
 
-    private ApiService apiService;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        handleAutoLogin();
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        handleAutoLogin();
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
     }
 
     private void handleAutoLogin() {
         SharedPreferences sp = getSharedPreferences("user_prefs", Context.MODE_PRIVATE);
         boolean rememberLogIn = sp.getBoolean("rememberLogIn", false);
 
-        if (rememberLogIn) {
-            SecureStorage ss = new SecureStorage();
-            LoginDTO credentials = ss.getUserCredentials(this);
-            if (credentials.getUsername() == null || credentials.getPassword() == null) {
-                sp.edit().putBoolean("rememberLogIn", false).apply();
-                Toast.makeText(this, "Error, por favor inica sesión manualmente", Toast.LENGTH_SHORT).show();
-                return;
+        if (!rememberLogIn) return;
+
+        SecureStorage ss = new SecureStorage();
+        LoginDTO credentials = ss.getUserCredentials(this);
+
+        if (credentials.getUsername() == null || credentials.getPassword() == null) {
+            sp.edit().putBoolean("rememberLogIn", false).apply();
+            Toast.makeText(this, "Error, por favor ingresa tus datos", Toast.LENGTH_SHORT).show();
+            Log.e(TAG, "Error handling autologin: stored credentials are null");
+            return;
+        }
+
+        Call<User> call = RetrofitClient.getApiService().logInUser(credentials);
+        call.enqueue(new Callback<User>() {
+            @Override
+            public void onResponse(@NonNull Call<User> call, @NonNull Response<User> response) {
+                if (response.isSuccessful() && response.body() != null) {
+                    User user = response.body();
+                    CurrentUser.setCurrentUser(user);
+                    Toast.makeText(getBaseContext(), "Inicio de sesión exitoso", Toast.LENGTH_SHORT).show();
+                    navigateToHomeActivity();
+                } else {
+                    sp.edit().putBoolean("rememberLogIn", false).apply();
+                    Log.e(TAG, "Error handling autologin: Server could not authenticate");
+                    Toast.makeText(getBaseContext(), "No pudimos iniciar sesión, intenta nuevamente", Toast.LENGTH_SHORT).show();
+                }
             }
 
-            Call<User> call = RetrofitClient.getApiService().logInUser(credentials);
-            call.enqueue(new Callback<User>() {
-                @Override
-                public void onResponse(@NonNull Call<User> call, @NonNull Response<User> response) {
-                    if (response.isSuccessful() && response.body() != null) {
-                        User user = response.body();
-                        CurrentUser.setCurrentUser(user);
-                        navigateToHomeActivity();
-                    } else {
-                        sp.edit().putBoolean("rememberLogIn", false).apply();
-                        Toast.makeText(getBaseContext(), "Error, por favor inica sesión manualmente", Toast.LENGTH_SHORT).show();
-                    }
-                }
+            @Override
+            public void onFailure(@NonNull Call<User> call, @NonNull Throwable t) {
+                Log.e(TAG, "Error handling autologin: " + t.getMessage());
+                Toast.makeText(getBaseContext(), "Error de conexión", Toast.LENGTH_SHORT).show();
+            }
+        });
 
-                @Override
-                public void onFailure(@NonNull Call<User> call, @NonNull Throwable t) {
-                    Toast.makeText(getBaseContext(), "Error de conexión", Toast.LENGTH_SHORT).show();
-                }
-            });
-        } else {
-            replaceFragment(new LogInFragment());
-        }
     }
 
     public void navigateToHomeActivity() {
